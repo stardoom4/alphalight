d84620455d6a499b6af2640267de505f94b06252
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-05-24 18:02:00 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Wunder',
+			customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+      ],
 			social: {
 				github: 'https://github.com/stardoom4/alphalight',
 			},
