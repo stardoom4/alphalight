993127ff2cc5491d993feb24155a391db0aa4f6b
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-05-24 16:55:47 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,10 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				{
+					label: 'Celestial Entity',
+					autogenerate: { directory: 'celestial-entity' },
+				},
 			],
 		}),
 	],
