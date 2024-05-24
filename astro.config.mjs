a1e700770dd2d94fe5c111aadeca4654412c9c0d
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-05-24 17:22:14 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,17 +11,6 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
 					label: 'Celestial Entity',
 					autogenerate: { directory: 'celestial-entity' },
 				},
